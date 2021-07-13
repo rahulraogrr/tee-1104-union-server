f7291b744a124c668111f1c080844440750567d8
Rahul Rao Gonda <rahulrao.grr@gmail.com>
2021-07-13 13:11:52 +0530

    Commit 2

---
+++ src/main/java/com/tee/union/Tee1104UnionServerApplication.java
@@ -0,0 +1,13 @@
+package com.tee.union;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tee1104UnionServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tee1104UnionServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tee/union/Tee1104UnionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tee.union;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tee1104UnionServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
